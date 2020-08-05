54b438fa1bb2ea39523ac61542f2b971844003f3
Naveksha Jain <naveksha.jain@daffodilsw.com>
2020-08-05 20:29:26 +0530

    Updates

---
+++ precache-manifest.1c2c9cccb740436d19f2120b88ea936c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6542f3e69945bb1d42d066c549dbd61b",
+    "url": "/educase/index.html"
+  },
+  {
+    "revision": "cddfcb53f51aafcf474a",
+    "url": "/educase/static/js/2.72eac5a6.chunk.js"
+  },
+  {
+    "revision": "3914ac2825c8beb5b984",
+    "url": "/educase/static/js/main.7d72058c.chunk.js"
+  },
+  {
+    "revision": "d9b8c53b491c0ed12c5f",
+    "url": "/educase/static/js/runtime~main.c5d48201.js"
+  }
+]);
